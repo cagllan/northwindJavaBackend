e48ff6c93d70f878518ed1930594ac62b83c738f
cagllan <caglanatay@windowslive.com>
2021-06-24 19:03:41 +0300

    Cors eklendi

--- src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
+++ src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +27,7 @@ import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping(value="/api/users")
+@CrossOrigin
 public class UsersController {
 	
 	private UserService userService;
